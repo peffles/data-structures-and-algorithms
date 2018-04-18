08d16af59399901dfc35cc24341bb19e99fdab0c
Wyatt  pefley <wyattpefley@Wyatts-MBP.localdomain>
2018-04-18 09:22:09 -0700

    code is jsfile

---
+++ shift-array/shiftArray.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let arrOne = [1,2,3,4,5];
+let arrTwo = [];
+let count = 0;
+
+function shiftArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    count++;
+  }
+  let middle = Math.Round(count/2);
+  for (let i = 0; i <= middle; i++); {
+    arrTwo.push[i];
+  }
+}
+
+shiftArray[1,2,3,4,5];
